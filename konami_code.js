33044208d32e199d0594db73e172cfbfd6af2669
Robert Meis <robert.meis@colorado.edu>
2017-11-06 21:23:30 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,11 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+const body = document.getElementById('body')
+
 function init() {
   // Write your JavaScript code inside the init() function
 
 }
+
+
+console.log("Hello")
